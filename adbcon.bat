59246b0705acd76942ddea6daff5fe28f309d104
adamsapple <hardrock.is.my.life@gmail.com>
2018-09-25 10:03:10 +0900

    初版追加

---
+++ adbcon.bat
@@ -0,0 +1,15 @@
+@echo off
+set devid=%1
+set port=%2
+REM adb shell "ip addr show wlan0 | grep 192.168 | sed -e 's/^.*inet[^6][^0-9]*\([0-9.]*\)[^0-9]*.*$/\1/'"
+
+@echo on
+adb -s %devid% tcpip %port%
+adb emu kill
+adb kill-server
+adb start-server
+
+for /f "usebackq tokens=*" %%i IN (`adb shell "ip addr show wlan0 | grep 192.168 | sed -e 's/^.*inet[^6][^0-9]*\([0-9.]*\)[^0-9]*.*$/\1/'"`) DO @set device_ip=%%i
+
+adb connect %device_ip%:%port%
+
